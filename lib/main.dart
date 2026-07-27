747196b01b3a06276738687bc245fe5d299ce518
Calebjazz <kalebumuladi@gmail.com>
2026-07-27 23:47:31 +0300

    structure#01

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-void main() async {
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'app.dart';
+import 'features/auth/presentation/bloc/auth_bloc.dart';
+import 'core/theme/bloc/theme_bloc.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // 1. Load the environment file first
   await dotenv.load(fileName: ".env");
 
-  // 2. Initialize Supabase by pulling the values dynamically
   await Supabase.initialize(
-    url: dotenv.env['SUPABASE_URL'] ?? '',
-    publishableKey: dotenv.env['SUPABASE_PUBLISHABLE_KEY'] ?? '',
+    url: dotenv.env['SUPABASE_URL']!,
+    publishableKey: dotenv.env['SUPABASE_PUBLISHABLE_KEY']!,
   );
 
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Miliki ERP',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Miliki ERP',
-            style: const TextStyle(
-              fontFamily: 'Poppins',
-            ),
-          ),
-          backgroundColor: const Color(0xFF00FF41),
-        ),
+  runApp(
+    BlocProvider(
+      create: (_) => ThemeBloc(),
+      child: BlocProvider(
+        create: (_) => AuthBloc(),
+        child: const MyApp(),
       ),
-    );
-  }
+    ),
+  );
 }
